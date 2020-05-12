0da180000306f7ad6d16a63c9a0dd3263a944321
Benjamin <benjamin.boboul@imt-lille-douai.org>
2020-05-13 00:14:36 +0200

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -3,18 +3,51 @@ import math
 import random
 
 # Grab the pellets as fast as you can!
+
 class Entity:
     def __init__(self, x, y):
         self.x: int = int(x)
         self.y: int = int(y)
 
+class Map:
+    def __init__(self, width, height):
+        self.width: int = width
+        self.height: int = height
+        self.entities = list()
+    
+    def append_entity(self, entity: Entity):
+        self.entities.append(entity)
+    
+    def __str__(self):
+        str_map = ""
+        #for x in range(self.width):
+            #for y in range(self.height):
+                # str_map+=self.entities[x][y]
+            #str_map+='\n'
+        return str(self.entities)
+
+
+class Wall(Entity):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+    def __repr__(self) -> str:
+        return "#"
+
 class Pellet(Entity):
-    def __init__(self, x, y, value):
+    def __init__(self, x: int, y: int, value: int):
         super().__init__(x, y)
         self.value = int(value)
 
     def __repr__(self) -> str:
         return f"Pellet(x:{self.x}, y:{self.y}, value:{self.value})"
+
+class Air(Pellet):
+    # Since pacman is able to pass through air we consider them as pellet of 0
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y, 0)
+    
+    def __repr__(self) -> str:
+        return " "
 
 class Pacman(Entity):
     def __init__(self, pac_id, mine, x, y, type_id, speed_turns_left, ability_cooldown):
@@ -28,18 +61,22 @@ class Pacman(Entity):
     def isMine(self) -> bool:
         return self.mine == 1
 
-    def isSomeoneInFront(self, scan_distance: int) -> bool:
-        return False
-
     def __repr__(self) -> str:
         return f"Pacman(x:{self.x}, y:{self.y})"
 
 # width: size of the grid
 # height: top left corner is (x=0, y=0)
 width, height = [int(i) for i in input().split()]
-for i in range(height):
+party_map = Map(width, height)
+for x in range(height):
     row = input()  # one line of the grid: space " " is floor, pound "#" is wall
-    print(row, file=sys.stderr)
+    for y, c in enumerate(list(row)):
+        if "#":
+            party_map.append_entity(Wall(x, y))
+        if " ":
+            party_map.append_entity(Air(x, y))
+
+print(str(party_map), file=sys.stderr)
 # game loop
 while True:
     my_score, opponent_score = [int(i) for i in input().split()]
