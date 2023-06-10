31e932f6759f01b4b95cde1509a180fec8c24084
Benjamin <8467845+benjaminBoboul@users.noreply.github.com>
2023-06-10 23:42:53 +0200

    refactor: small rust clean up

--- defibrillator/main.rs
+++ defibrillator/main.rs
@@ -1,12 +1,15 @@
 use std::io;
 
 macro_rules! parse_input {
-    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
 }
 
-
 fn as_float(s: &str) -> f64 {
-    return s.replace(',', ".").parse::<f64>().expect("Can't parse value {s}.");
+    s.replace(',', ".")
+        .parse::<f64>()
+        .expect("Can't parse value {s}.")
 }
 
 /**
@@ -19,10 +22,6 @@ fn main() {
 
     let lon = as_float(input_line.trim());
 
-
-    eprintln!("lon: {lon}");
-
-
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let lat = as_float(input_line.trim());
@@ -34,9 +33,7 @@ fn main() {
     let mut close_defibrilator = std::f64::MAX;
     let mut close_defibrilator_name: String = String::from("NONE");
 
-    eprintln!("close_defibrilator: {close_defibrilator}");
-
-    for i in 0..n as usize {
+    for _i in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let defib = input_line.trim_matches('\n').to_string();
@@ -44,19 +41,20 @@ fn main() {
         let lo = as_float(s_defib.get(4).expect("Can't read value lo.").to_owned());
         let la = as_float(s_defib.get(5).expect("Can't read value lo.").to_owned());
 
-        eprintln!("line: {defib}\nlo: {lo}\nla: {la}");
+        let x = (lo - lon) * ((lat + la) / 2.0).cos();
+        let y = la - lat;
 
-        let x = (lo - lon) * ((lat + la)/2.0).cos();
-        let y = (la - lat);
-        
-        let d  = (x*x + y*y).sqrt() * 6371.0;
+        let d = (x * x + y * y).sqrt() * 6371.0;
 
-        if (close_defibrilator > d) {
-            let name = s_defib.get(1).expect("Can't get defibrilator name.").to_owned().to_string();
-            close_defibrilator_name = String::from(name);
+        if close_defibrilator > d {
+            let name = s_defib
+                .get(1)
+                .expect("Can't get defibrilator name.")
+                .to_owned()
+                .to_string();
+            close_defibrilator_name = name;
             close_defibrilator = d;
         }
-        
     }
 
     // Write an answer using println!("message...");
